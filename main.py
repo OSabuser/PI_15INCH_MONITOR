55d8f5c0b9aa0c1db2a797739cd1f57df3fb5f25
Dmitry Akimov <von.akimov@gmail.com>
2022-10-28 16:21:28 +0300

    # TEST

--- main.py
+++ main.py
@@ -2,14 +2,11 @@
 
 import pyglet
 from pyglet import image
-from pyglet.media import Player
+from pyglet.media import Player, load
 from pyglet.window import Window
 from pyglet.sprite import Sprite
 from pyglet.gl import *
 
-
-# TODO: 1. try, except на критически важные блоки кода
-# TODO: 2. Статические изображения + анимации
 pyglet.options['search_local_libs'] = True
 
 path_to_video = "test_1024x768.mp4"
@@ -22,8 +19,8 @@ if __name__ == '__main__':
     background = pyglet.graphics.OrderedGroup(0)
     foreground = pyglet.graphics.OrderedGroup(1)
 
-    player = pyglet.media.Player()
-    mp4_file = pyglet.media.load(path_to_video)
+    player = Player()
+    mp4_file = load(path_to_video)
     player.queue(mp4_file)
     player.loop = True
     player.play()
@@ -31,8 +28,8 @@ if __name__ == '__main__':
     pic_img = Sprite(image.load('pic_4.png'), x=50, y=200, group=foreground)
     floor_img = Sprite(image.load('1.png'), x=750, y=300, group=foreground)
 
-    win = Window(width=1024, height=768, fullscreen=False)
-   # win.set_mouse_visible(visible=False)
+    win = Window(width=1024, height=768, fullscreen=True)
+    win.set_mouse_visible(visible=False)
 
     floor_state = ['0', '0']
     arrow_state = ['0', '0']
@@ -47,7 +44,7 @@ if __name__ == '__main__':
         if player.source and player.source.video_format:
             player.get_texture().blit(0, 0)
         pic_img.draw()
-        floor_img.draw()
+        #floor_img.draw()
 
     @win.event
     def on_draw():
