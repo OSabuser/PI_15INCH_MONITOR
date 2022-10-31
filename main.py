eee0679c4f1407d4a0b8f8be0f17e9cb1597c9a5
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 15:41:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,16 +24,17 @@ while True:
     try:
         ser = serial.Serial(port=uart0_port_name, baudrate=uart0_baud)  # open serial port
     except SerialException:
-        print('Serial port connection error!\n')
+        print(f'{uart0_port_name} НЕДОСТУПЕН!\n')
         pass
     else:
+        print(f'{uart0_port_name} В РАБОТЕ!\n')
         break
 
 
 def update_floor_img(state):
     global floor_l_surface, floor_r_surface, floor_l_layer, floor_r_layer
 
-    if state[0] is not state[1]:  # Draw floor number
+    if state[0] is not state[1]:
 
         redraw_need = True if (state[0] // 10) != (state[1] // 10) else False
 
@@ -60,7 +61,7 @@ def update_floor_img(state):
                 image = pygame.image.load(f'images/{state[0] // 10}.png')
                 floor_l_surface.blit(image, floor_l_pos)
 
-        # Trigger the redraw of the screen from the buffer
+        # Обновление программного слоя в GPU
         if redraw_need:
             floor_l_layer.updateLayer()
         floor_r_layer.updateLayer()
@@ -69,41 +70,61 @@ def update_floor_img(state):
 
 
 def update_mode_img(state):
+    global icon_surface, icon_layer
+
     if state[0] is not state[1]:  # Draw icon image
 
-        # Trigger the redraw of the screen from the buffer
+        del icon_surface
+        del icon_layer
+
+        icon_layer = pydispmanx.dispmanxLayer(3)
+        icon_surface = pygame.image.frombuffer(icon_layer, icon_layer.size, 'RGBA')
+
+        if state[0] == "UP":
+            image = pygame.image.load('images/ARROW_UP.png')
+            icon_surface.blit(image, icon_pos)
+        elif state[0] == "DL":
+            image = pygame.image.load('images/ARROW_DOWN.png')
+            icon_surface.blit(image, icon_pos)
+
         icon_layer.updateLayer()
-        image = pygame.image.load('images/ARROW_UP.png')
-        icon_surface.blit(image, icon_pos)
 
     return state[0]
 
 
 # Список допустимых номеров этажей
-ok_list = list(map(str, range(allowable_floor_range[0], allowable_floor_range[1] + 1)))
-for idx, element in enumerate(ok_list):
-    ok_list[idx] = element.rjust(2, '0')
-    
+floor_list = list(map(str, range(allowable_floor_range[0], allowable_floor_range[1] + 1)))
+for idx, element in enumerate(floor_list):
+    floor_list[idx] = element.rjust(2, '0')
+
+mode_list = ("NN", "DL", "UP")
+
 floor_number, mode = '', ''
-floor_state, arrow_state = [0, 0], [0, 0]
+floor_state = [0, 0]
+arrow_state = ["", ""]
+
 message_received = False
 
 while True:
 
-    # UART message handling from MCU
+    # Обработка сообщения, принятого по UART от STM32
     if ser.inWaiting() > 0:
-        # read the bytes and convert from binary array to ASCII
         data_str = ser.read(ser.inWaiting()).decode('ascii')
         # For debug purposes -- > print(data_str)
         if len(data_str) == 6:
             floor_number = data_str[1:3]  # Get floor number
-            mode = data_str[3:5]  # Get direction state
+            mode = data_str[4:6]  # Get direction state
             message_received = True
             print(f"Floor: {floor_number}, mode: {mode}")
 
+    # Отрисовка изображений
     if message_received:
         message_received = False
-        if floor_number in ok_list:
+        if floor_number in floor_list:
             floor_state[0] = int(floor_number)
             print(f"RAW FLOOR: {floor_state[0]}")
             floor_state[1] = update_floor_img(floor_state)
+
+        if mode in mode_list:
+            arrow_state[0] = mode
+            print(f"RAW MODE: {arrow_state[0]}")
