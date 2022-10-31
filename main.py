31fa88e65fccd3cfeb62758f04540e9ad3410e67
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 14:31:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pydispmanx, pygame, time
 floor_r_pos = (250, 284)
 floor_l_pos = (100, 284)
 icon_pos = (600, 284)
+allowable_floor_range = (1, 40)
 
 # Create new layer object for GPU layer 1
 floor_l_layer = pydispmanx.dispmanxLayer(1)
@@ -14,23 +15,52 @@ floor_l_surface = pygame.image.frombuffer(floor_l_layer, floor_l_layer.size, 'RG
 floor_r_surface = pygame.image.frombuffer(floor_r_layer, floor_r_layer.size, 'RGBA')
 icon_surface = pygame.image.frombuffer(icon_layer, icon_layer.size, 'RGBA')
 
-image = pygame.image.load('images/6.png')
-floor_r_surface.blit(image, floor_r_pos)
+def update_floor_img(state):
+    global floor_l_surface, floor_r_surface
+    if state[0] is not state[1]:  # Draw floor number
 
-image = pygame.image.load('images/9.png')
-floor_l_surface.blit(image, floor_l_pos)
+        #del floor_l_surface
+        #del floor_r_surface
 
-image = pygame.image.load('images/ARROW_UP.png')
-icon_surface.blit(image, icon_pos)
+        if state[0] < 10:
+            image = pygame.image.load(f'images/{state[0]}.png')
+            floor_r_surface.blit(image, floor_r_pos)
+        elif state[0] < allowable_floor_range[1] + 1:
+            image = pygame.image.load(f'images/{state[0] % 10}.png')
+            floor_r_surface.blit(image, floor_r_pos)
+            image = pygame.image.load(f'images/{state[0] / 10}.png')
+            floor_l_surface.blit(image, floor_l_pos)
 
-# Trigger the redraw of the screen from the buffer
-icon_layer.updateLayer()
-floor_l_layer.updateLayer()
-floor_r_layer.updateLayer()
+        # Trigger the redraw of the screen from the buffer
+        floor_l_layer.updateLayer()
+        floor_r_layer.updateLayer()
+
+    return state[0]
+
+
+def update_mode_img(state):
+    if state[0] is not state[1]:  # Draw icon image
+
+        # Trigger the redraw of the screen from the buffer
+        icon_layer.updateLayer()
+        image = pygame.image.load('images/ARROW_UP.png')
+        icon_surface.blit(image, icon_pos)
+
+    return state[0]
+
+
+cnt = 0
+floor_state = [1, 0]
+arrow_state = [0, 0]
+# Список допустимых номеров этажей
+ok_list = list(map(str, range(allowable_floor_range[0], allowable_floor_range[1] + 1)))
+
 while True:
-    print("Work flow!")
-    print(f'Layer size:{icon_layer.size}')
-    time.sleep(0.5)
+    floor_state[1] = update_floor_img(floor_state)
+    floor_state[0] += 1
+    if floor_state[0] == 40:
+        floor_state[0] = 1
+    time.sleep(3.5)
 # Do other things, redraw layers etc
 
 # Delete surface before layer
