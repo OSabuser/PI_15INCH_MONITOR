ad77a2e0033200e33206b4ab767a011b861e8554
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 12:45:06 +0300

    # BEGIN

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import pydispmanx, pygame, time
 
 # Create new layer object for GPU layer 1
-demolayer = pydispmanx.dispmanxLayer(1)
-# Create pyGame surface linked to the layer buffer
-demoSurface = pygame.image.frombuffer(demolayer, demolayer.size, 'RGBA')
+floor_layer = pydispmanx.dispmanxLayer(1)
+icon_layer = pydispmanx.dispmanxLayer(1)
 
-# Use exsisting pyGame features to draw to the surface
-# Choosing red color for the rectangle
-color = (255, 255, 0)
+# Create pyGame surfaces linked to the layer buffers
+floor_surface = pygame.image.frombuffer(floor_layer, floor_layer.size, 'RGBA')
+icon_surface = pygame.image.frombuffer(icon_layer, icon_layer.size, 'RGBA')
 
 # Creating the image surface
-image = pygame.image.load('pic_4.png')
+image = pygame.image.load('1.png')
+floor_surface.blit(image, (550, 150))
 
 # putting our image surface on display
 # surface
-demoSurface.blit(image, (650, 150))
+image = pygame.image.load('8.png')
+icon_surface.blit(image, (100, 150))
 
 # Trigger the redraw of the screen from the buffer
-demolayer.updateLayer()
+icon_layer.updateLayer()
+floor_layer.updateLayer()
 
 while True:
     print("Work flow!")
-    print(f'Layer size:{demolayer.size}')
+    print(f'Layer size:{icon_layer.size}')
     time.sleep(0.5)
 # Do other things, redraw layers etc
 
