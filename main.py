03a90ab056f3802e392f7bc2884e5c0e008da65e
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 11:24:42 +0300

    ##

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import time
-from random import randint
-import numpy
 from PIL import Image, ImageDraw
 from dispmanx import DispmanX
 
 
-def random_color_with_alpha():
-    return tuple(randint(0, 0xFF) for _ in range(3)) + (randint(0x44, 0xFF),)
+display = DispmanX(layer=1, pixel_format="RGBA", buffer_type="numpy")
+display.width = 400
+display.height = 400
 
-
-display = DispmanX(pixel_format="RGBA", buffer_type="numpy")
-image = Image.new(mode=display.pixel_format, size=display.size)
+image = Image.open('pic_4.png')
 draw = ImageDraw.Draw(image)
 
-for _ in range(20):
-    draw.rectangle(((0, 0), image.size), fill=random_color_with_alpha())
-    numpy.copyto(display.buffer, image)
+while True:
+    print(f"Work state!")
     display.update()
-    time.sleep(0.5)
+    time.sleep(1.0)
