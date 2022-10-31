b57f5e83d7633304ea821d6748a100dd31abf8d6
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 13:01:24 +0300

    # 2

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import pydispmanx, pygame, time
 
+floor_r_pos = (250, 284)
+floor_l_pos = (100, 284)
+icon_pos = (600, 284)
+
 # Create new layer object for GPU layer 1
-floor_layer = pydispmanx.dispmanxLayer(1)
-icon_layer = pydispmanx.dispmanxLayer(1)
+floor_l_layer = pydispmanx.dispmanxLayer(1)
+floor_r_layer = pydispmanx.dispmanxLayer(2)
+icon_layer = pydispmanx.dispmanxLayer(3)
 
 # Create pyGame surfaces linked to the layer buffers
-floor_surface = pygame.image.frombuffer(floor_layer, floor_layer.size, 'RGBA')
+floor_l_surface = pygame.image.frombuffer(floor_l_layer, floor_l_layer.size, 'RGBA')
+floor_r_surface = pygame.image.frombuffer(floor_r_layer, floor_r_layer.size, 'RGBA')
 icon_surface = pygame.image.frombuffer(icon_layer, icon_layer.size, 'RGBA')
 
-# Creating the image surface
-image = pygame.image.load('1.png')
-floor_surface.blit(image, (550, 150))
+image = pygame.image.load('/images/6.png')
+floor_r_surface.blit(image, floor_r_pos)
 
-# putting our image surface on display
-# surface
-image = pygame.image.load('8.png')
-icon_surface.blit(image, (100, 150))
+image = pygame.image.load('/images/9.png')
+floor_l_surface.blit(image, floor_l_pos)
+
+image = pygame.image.load('/images/ARROW_UP.png')
+icon_surface.blit(image, icon_pos)
 
 # Trigger the redraw of the screen from the buffer
 icon_layer.updateLayer()
-floor_layer.updateLayer()
-
+floor_l_layer.updateLayer()
+floor_r_layer.updateLayer()
 while True:
     print("Work flow!")
     print(f'Layer size:{icon_layer.size}')
