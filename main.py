dbce6c285123cde9010b067a57b7350fe68c9770
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 11:28:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw
 from dispmanx import DispmanX
 
 
-display = DispmanX(layer=1, pixel_format="RGBA", buffer_type="auto")
+display = DispmanX(pixel_format="RGBA", buffer_type="auto")
 
 image = Image.open('pic_4.png')
 draw = ImageDraw.Draw(image)
