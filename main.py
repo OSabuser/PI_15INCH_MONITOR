28160e6ab0352165a1b35dd8ed9f47504e357ad3
Dmitry Akimov <von.akimov@gmail.com>
2022-10-31 11:28:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ display = DispmanX(layer=1, pixel_format="RGBA", buffer_type="auto")
 
 image = Image.open('pic_4.png')
 draw = ImageDraw.Draw(image)
+display.update()
 
 while True:
     print(f"Work state!")
-    display.update()
     time.sleep(1.0)
